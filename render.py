880b160ad31033150eb4b0aaf7be9f892d9da5f1
HU Pili <hpl1989@gmail.com>
2014-07-15 20:22:05 +0800

    add newsletter description

--- render.py
+++ render.py
@@ -9,10 +9,13 @@ from jinja2 import Template
 import requests
 
 url_source_json = 'https://spreadsheets.google.com/feeds/list/1doNM4JLsUD33uFSRDeqEPd8XbnTlelm8A4SG7akOAFM/od6/public/values?alt=json'
+issue_date = 'July 18, 2014'
 
 data = {}
 res = requests.get(url_source_json).json()
 data['entries'] = res['feed']['entry']
+
+data['issue_date'] = issue_date
 
 template = Template(open('template.tpl.html').read())
 rendered = template.render(**data)
