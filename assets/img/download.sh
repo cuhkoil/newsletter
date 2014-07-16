61d739dd850a077b831da86f0229e8aae87f5465
HU Pili <hpl1989@gmail.com>
2014-07-16 15:56:37 +0800

    add more choices

--- assets/img/download.sh
+++ assets/img/download.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-wget 'https://docs.google.com/drawings/d/1VEoRTyumJIP2W06e0YV7w12ODY0p0wkZvrMbT-BkAPc/pub?w=320&h=320' -O logo.png
+url_logo_template='https://docs.google.com/drawings/d/1VEoRTyumJIP2W06e0YV7w12ODY0p0wkZvrMbT-BkAPc/pub?' 
+wget ${url_logo_template}"w=320&h=320" -O logo-320.png
+wget ${url_logo_template}"w=160&h=160" -O logo-160.png
+wget ${url_logo_template}"w=80&h=80" -O logo-80.png
