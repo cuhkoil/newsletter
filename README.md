fcc7aefe88aebc0e1e6b1e38506054db14233db8
HU Pili <hpl1989@gmail.com>
2014-07-16 18:34:15 +0800

    add pdf and jpg format

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Contains:
    * Rendering logic
 
 The data source is hosted on Google spreadsheets
+
+## Dependencies
+
+   * `pip install -r requirements.txt`
+   * https://github.com/wkhtmltopdf/wkhtmltopdf
+   * pandoc
--- format.sh
+++ format.sh
@@ -4,5 +4,8 @@ for fn in `ls -1 issue/ | grep ".html" | sed 's/.html//'`
 do
     echo $fn
     pandoc -f html -t plain -o issue/${fn}.txt issue/${fn}.html
+    # Pandoc can not fetch remote images specified with abs URL
     #pandoc -f html -t pdf -o issue/${fn}.pdf issue/${fn}.html
+    wkhtmltopdf --page-width 680 issue/${fn}.html issue/${fn}.pdf
+    wkhtmltoimage --disable-smart-width --width 680 issue/${fn}.html issue/${fn}.jpg
 done
