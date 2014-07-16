3d3dc32860cc6bb966a9a98e7a5734c8a132db36
HU Pili <hpl1989@gmail.com>
2014-07-16 16:11:15 +0800

    embed the logo

--- assets/img/download.sh
+++ assets/img/download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 url_logo_template='https://docs.google.com/drawings/d/1VEoRTyumJIP2W06e0YV7w12ODY0p0wkZvrMbT-BkAPc/pub?' 
+wget ${url_logo_template}"w=640&h=640" -O logo-640.png
 wget ${url_logo_template}"w=320&h=320" -O logo-320.png
 wget ${url_logo_template}"w=160&h=160" -O logo-160.png
 wget ${url_logo_template}"w=80&h=80" -O logo-80.png
--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import base64
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
@@ -10,12 +11,14 @@ import requests
 
 url_source_json = 'https://spreadsheets.google.com/feeds/list/1doNM4JLsUD33uFSRDeqEPd8XbnTlelm8A4SG7akOAFM/od6/public/values?alt=json'
 issue_date = 'July 18, 2014'
+logo_base64 = base64.b64encode(open('assets/img/logo-320.png').read())
 
 data = {}
 res = requests.get(url_source_json).json()
 data['entries'] = res['feed']['entry']
 
 data['issue_date'] = issue_date
+data['logo_base64'] = logo_base64
 
 template = Template(open('template.tpl.html').read())
 rendered = template.render(**data)
